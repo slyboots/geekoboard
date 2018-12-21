07893aeacfecfb695bff6998cf00645e58ed581c
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-21 13:32:21 -0600

    updated app flow and added reset param

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from geekoapi import app
+import sys
+import json
+from geekoapi import app, schedules
 
-app.ping()
+#set_schedule(**EXAMPLE_DATA_STARTEND_TIMES)
+#set_scheduleset(EXAMPLE_DATA)
+#delete_schedule()
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'reset':
+        app.delete_schedule()
+        sys.exit()
+print(f"Getting {app.DATASET_NAME} dataset")
+schedule_dataset = app.get_schedule(schedules.SCHEDULE_SCHEMA)
+print(f"Getting current schedule state")
+current_schedule = schedules.as_dataset()
+print(f"Current schedule state:\n{json.dumps(current_schedule, indent=2)}")
+app.set_schedule(current_schedule, schedules.SCHEDULE_SCHEMA)
+print("Dataset updated!")
