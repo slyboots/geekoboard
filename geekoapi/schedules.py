ea7443b20dfaebced3c0110468f48b07264ed77f
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-21 13:32:27 -0600

    updated schedule schema

--- geekoapi/schedules.py
+++ geekoapi/schedules.py
@@ -11,10 +11,15 @@ SCHEDULE_SCHEMA = {
         'type': 'string',
         'name': "Group",
         'optional': False
+    },
+    'online': {
+        'type': 'number',
+        'name': 'Status',
+        'optional': True
     }
 }
 
 
 def as_dataset():
     raw_schedule = current_agent_statuses()
-    return [{'agent': k, 'group': v} for k, v in raw_schedule.items()]
+    return [{'agent': k.upper(), 'group': v.upper(), 'online': 0} for k, v in raw_schedule.items()]
