53f487c20700057c54c55a7b3eb13d787741d2cf
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-21 12:13:23 -0600

    added methods for handling datasets

--- geekoapi/app.py
+++ geekoapi/app.py
@@ -1,11 +1,29 @@
 import os
+import json
 import geckoboard
 
+
+DATASET_NAME = 'agents.active_groups'
 CLIENT = geckoboard.client(os.getenv('GECKO_API_KEY'))
 
-def ping():
-    try:
-        CLIENT.ping()
-        print('Authentication successful')
-    except:
-        print('Incorrect API key')
+
+try:
+    CLIENT.ping()
+except:
+    raise PermissionError('API key invalid.')
+
+
+def get_schedule(schema):
+    print(f"Retrieving gecko dataset: {DATASET_NAME}")
+    return CLIENT.datasets.find_or_create(DATASET_NAME, schema)
+
+
+def set_schedule(data, schema):
+    print(f"Setting schedule with data:\n{json.dumps(data,indent=2)}")
+    schedule = get_schedule(schema)
+    schedule.put(data)
+
+
+def delete_schedule():
+    print(f"Deleting gecko dataset: {DATASET_NAME}")
+    CLIENT.datasets.delete(DATASET_NAME)
