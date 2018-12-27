db51c454ca2762765c6bcb0831017803dbe777e0
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-27 14:45:53 -0600

    timezones fucked me up

--- geekoboard/app.py
+++ geekoboard/app.py
@@ -9,7 +9,9 @@ from google.oauth2 import service_account
 
 # disable google api warnings
 logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
+
 # VARS
+DEBUG = os.getenv('DEBUG') == 'True'
 CLIENT = geckoboard.client(os.getenv('GECKO_API_KEY'))
 DATASET_NAME = 'agents.active_groups'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -36,10 +38,21 @@ SCHEDULE_SCHEMA = {
     }
 }
 
+def get_service():
+    creds = service_account.Credentials.from_service_account_file(CREDENTIALS, scopes=SCOPES)
+    service = build('sheets', 'v4', credentials=creds)
+    return service.spreadsheets()
+
+
+GS_SERVICE = get_service()
+
 
 def current_hour():
     '''gets the current hour out of 24 as an int'''
-    return datetime.datetime.now().hour
+    current = datetime.datetime.now(datetime.timezone(-datetime.timedelta(hours=6))).hour
+    if DEBUG:
+        print(f"lambda.current_hour: {current}")
+    return current
 def to_24hour(hourstring):
     '''converts a time string like 8AM or 5PM to an int representing its 24 hour value'''
     offset = 12 if all(x not in hourstring for x in['12', 'AM']) else 0
@@ -47,11 +60,13 @@ def to_24hour(hourstring):
 def format_dataset(raw_schedule):
     return [{'agent': k.upper(), 'group': v.upper(), 'online': 0} for k, v in raw_schedule.items()]
 
+
 def get_schedule(schema):
     print(f"Retrieving gecko dataset: {DATASET_NAME}")
     return CLIENT.datasets.find_or_create(DATASET_NAME, schema)
 def set_schedule(data, schema):
-    print(f"Setting schedule with data:\n{json.dumps(data)}")
+    if DEBUG:
+        print(f"Setting schedule with data:\n{json.dumps(data)}")
     schedule = get_schedule(schema)
     schedule.put(data)
 def delete_schedule():
@@ -59,27 +74,28 @@ def delete_schedule():
     CLIENT.datasets.delete(DATASET_NAME)
 
 
-def get_service():
-    creds = service_account.Credentials.from_service_account_file(CREDENTIALS, scopes=SCOPES)
-    service = build('sheets', 'v4', credentials=creds)
-    return service.spreadsheets()
 def get_schedule_timeline(sheets=None):
     if not sheets:
-        sheets = get_service()
+        sheets = GS_SERVICE
     timeline_range = sheets.values().get(spreadsheetId=SHEET_ID, range=SCHEDULE_TIMELINE_RANGE).execute()
     values = timeline_range.get('values', [])
     if not values:
         raise ValueError(f"no data found for {SCHEDULE_TIMELINE_RANGE} range")
     else:
-        return [to_24hour(x) for x in values[0]]
+        timeline_formatted = [to_24hour(x) for x in values[0]]
+        if DEBUG:
+            print(f"lambda.get_schedule_timeline: {json.dumps(timeline_formatted)}")
+        return timeline_formatted
 def get_agent_schedules(sheets=None):
     if not sheets:
-        sheets = get_service()
+        sheets = GS_SERVICE
     schedules_range = sheets.values().get(spreadsheetId=SHEET_ID, range=AGENT_SCHEDULES_RANGE).execute()
     values = schedules_range.get('values', [])
     if not values:
         raise ValueError(f"no data found for {AGENT_SCHEDULES_RANGE} range")
     else:
+        if DEBUG:
+            print(f"lambda.get_agent_schedules(): {json.dumps(values)}")
         return values
 def current_agent_statuses():
     agent_statuses = {}
@@ -88,10 +104,14 @@ def current_agent_statuses():
     for row in schedules:
         agent = row[0]
         schedule = row[1:]
+        if DEBUG:
+            print(f"lambda.current_agent_statuses: block={timeblock} {agent} schedule: {json.dumps(schedule)}")
         try:
             agent_statuses[agent] = schedule[timeblock]
         except IndexError:
             agent_statuses[agent] = ""
+    if DEBUG:
+        print('Agent statuses: %s', json.dumps(agent_statuses))
     return agent_statuses
 
 
@@ -104,8 +124,6 @@ def lambda_handler(event, context):
         if sys.argv[1] == 'reset':
             delete_schedule()
             sys.exit()
-    print(f"Getting {DATASET_NAME} dataset")
-    schedule_dataset = get_schedule(SCHEDULE_SCHEMA)
     print(f"Getting current schedule state")
     current_schedule = format_dataset(current_agent_statuses())
     print(f"Current schedule state:\n{json.dumps(current_schedule)}")
