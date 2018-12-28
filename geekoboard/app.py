1b89d72271ef3f76ae4e9222a4920c9f7988c2ba
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-12-28 11:46:26 -0600

    Zendesk agent call status is pulled in now!!!
    also made some changes to the code formatting

--- geekoboard/app.py
+++ geekoboard/app.py
@@ -4,6 +4,7 @@ import json
 import logging
 import datetime
 import geckoboard
+import requests
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
@@ -12,15 +13,19 @@ logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
 
 # VARS
 DEBUG = os.getenv('DEBUG') == 'True'
-CLIENT = geckoboard.client(os.getenv('GECKO_API_KEY'))
-DATASET_NAME = 'agents.active_groups'
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-CREDENTIALS = os.getenv('GOOGLE_SHEETS_SERVICE_CREDENTIALS')
-TOKEN = os.getenv('GOOGLE_SHEETS_TOKEN')
-SHEET_ID = os.getenv('SCHEDULE_SHEET_ID')
+GOOGLE_API_SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+GOOGLE_SHEETS_SERVICE_CREDENTIALS = os.getenv('GOOGLE_SHEETS_SERVICE_CREDENTIALS')
+SCHEDULE_SHEET_ID = os.getenv('SCHEDULE_SHEET_ID')
 AGENT_SCHEDULES_RANGE = 'AgentSchedules'
 SCHEDULE_TIMELINE_RANGE = 'ScheduleTimeline'
-SCHEDULE_SCHEMA = {
+ZD_API_URL = os.getenv('ZD_API_URL')
+ZD_API_USER = os.getenv('ZD_API_USER')
+ZD_API_KEY = os.getenv('ZD_API_KEY')
+ZD_GROUP_ID = os.getenv('ZD_GROUP_ID')
+ZD_API_TALK_ENDPOINT = os.getenv('ZD_API_TALK_ENDPOINT')
+GECKO_CLIENT = geckoboard.client(os.getenv('GECKO_API_KEY'))
+GECKO_DATASET_NAME = 'agents.active_groups'
+GECKO_DATA_SCHEMA = {
     'agent': {
         'type': 'string',
         'name': 'Agent',
@@ -31,103 +36,128 @@ SCHEDULE_SCHEMA = {
         'name': "Group",
         'optional': False
     },
-    'online': {
-        'type': 'number',
+    'status': {
+        'type': 'string',
         'name': 'Status',
-        'optional': True
+        'optional': False
     }
 }
 
+
 def get_service():
-    creds = service_account.Credentials.from_service_account_file(CREDENTIALS, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(GOOGLE_SHEETS_SERVICE_CREDENTIALS, scopes=GOOGLE_API_SCOPES)
     service = build('sheets', 'v4', credentials=creds)
     return service.spreadsheets()
 
 
 GS_SERVICE = get_service()
 
-
+# GENERAL STUFF
 def current_hour():
     '''gets the current hour out of 24 as an int'''
-    current = datetime.datetime.now(datetime.timezone(-datetime.timedelta(hours=6))).hour
-    if DEBUG:
-        print(f"lambda.current_hour: {current}")
-    return current
+    return datetime.datetime.now(datetime.timezone(-datetime.timedelta(hours=6))).hour
 def to_24hour(hourstring):
     '''converts a time string like 8AM or 5PM to an int representing its 24 hour value'''
     offset = 12 if all(x not in hourstring for x in['12', 'AM']) else 0
     return (int(hourstring[:-2])+offset)%24
 def format_dataset(raw_schedule):
-    return [{'agent': k.upper(), 'group': v.upper(), 'online': 0} for k, v in raw_schedule.items()]
+    return [{'agent': k.upper(), 'group': v.upper() or 'OTHER', 'status': ''} for k, v in raw_schedule.items()]
+# END GENERAL STUFF
 
-
-def get_schedule(schema):
-    print(f"Retrieving gecko dataset: {DATASET_NAME}")
-    return CLIENT.datasets.find_or_create(DATASET_NAME, schema)
-def set_schedule(data, schema):
-    if DEBUG:
-        print(f"Setting schedule with data:\n{json.dumps(data)}")
-    schedule = get_schedule(schema)
+# GECKBOARD STUFF
+def get_gecko_dataset():
+    print(f"Retrieving gecko dataset: {GECKO_DATASET_NAME}")
+    return GECKO_CLIENT.datasets.find_or_create(GECKO_DATASET_NAME, GECKO_DATA_SCHEMA)
+def set_gecko_dataset(data):
+    print(f"Setting gecko dataset with data:\n{json.dumps(data)}")
+    schedule = get_gecko_dataset()
     schedule.put(data)
-def delete_schedule():
-    print(f"Deleting gecko dataset: {DATASET_NAME}")
-    CLIENT.datasets.delete(DATASET_NAME)
+def del_gecko_dataset():
+    print(f"Deleting gecko dataset: {GECKO_DATASET_NAME}")
+    GECKO_CLIENT.datasets.delete(GECKO_DATASET_NAME)
+# END GECKOBOARD STUFF
 
-
-def get_schedule_timeline(sheets=None):
-    if not sheets:
-        sheets = GS_SERVICE
-    timeline_range = sheets.values().get(spreadsheetId=SHEET_ID, range=SCHEDULE_TIMELINE_RANGE).execute()
+# GOOGLE SHEETS STUFF
+def get_schedule_timeline():
+    timeline_range = GS_SERVICE.values().get(spreadsheetId=SCHEDULE_SHEET_ID, range=SCHEDULE_TIMELINE_RANGE).execute()
     values = timeline_range.get('values', [])
     if not values:
         raise ValueError(f"no data found for {SCHEDULE_TIMELINE_RANGE} range")
-    else:
-        timeline_formatted = [to_24hour(x) for x in values[0]]
-        if DEBUG:
-            print(f"lambda.get_schedule_timeline: {json.dumps(timeline_formatted)}")
-        return timeline_formatted
-def get_agent_schedules(sheets=None):
-    if not sheets:
-        sheets = GS_SERVICE
-    schedules_range = sheets.values().get(spreadsheetId=SHEET_ID, range=AGENT_SCHEDULES_RANGE).execute()
+    timeline_formatted = [to_24hour(x) for x in values[0]]
+    DEBUG and print(f"get_schedule_timeline: {json.dumps(timeline_formatted)}")
+    return timeline_formatted
+def get_agent_schedules():
+    schedules_range = GS_SERVICE.values().get(spreadsheetId=SCHEDULE_SHEET_ID, range=AGENT_SCHEDULES_RANGE).execute()
     values = schedules_range.get('values', [])
+    DEBUG and print(f"get_agent_schedules: {json.dumps(values)}")
     if not values:
         raise ValueError(f"no data found for {AGENT_SCHEDULES_RANGE} range")
-    else:
-        if DEBUG:
-            print(f"lambda.get_agent_schedules(): {json.dumps(values)}")
-        return values
+    return values
 def current_agent_statuses():
     agent_statuses = {}
     timeblock = get_schedule_timeline().index(current_hour())
     schedules = get_agent_schedules()
     for row in schedules:
-        agent = row[0]
-        schedule = row[1:]
-        if DEBUG:
-            print(f"lambda.current_agent_statuses: block={timeblock} {agent} schedule: {json.dumps(schedule)}")
+        agent, schedule = row[0], row[1:]
         try:
             agent_statuses[agent] = schedule[timeblock]
         except IndexError:
             agent_statuses[agent] = ""
-    if DEBUG:
-        print('Agent statuses: %s', json.dumps(agent_statuses))
+    DEBUG and print(f"Agent statuses: {json.dumps(agent_statuses)}")
     return agent_statuses
+# END GOOGLE SHEETS STUFF
+
+# ZENDESK STUFF
+def format_call_status(status):
+    status_map = {
+        'on_call': 'BUSY',
+        'not_available': 'OFFLINE',
+        'available': 'ONLINE',
+        'wrap_up': 'WRAPUP'
+    }
+    return status_map.get(status)
+def get_zendesk_talk_status(agent_id):
+    talk_api = f"{ZD_API_URL}channels/voice/availabilities/{agent_id}.json"
+    auth = requests.auth.HTTPBasicAuth(ZD_API_USER, ZD_API_KEY)
+    zd_request = requests.get(talk_api, auth=auth)
+    return format_call_status(zd_request.json()["availability"]["status"])
+def get_zendesk_support_agents():
+    support_agents = {}
+    support_api = f"{ZD_API_URL}/users.json?role[]=agent&role[]=admin"
+    auth = requests.auth.HTTPBasicAuth(ZD_API_USER, ZD_API_KEY)
+    zd_request = requests.get(support_api, auth=auth)
+    users = zd_request.json()
+    for user in users['users']:
+        default_name = user['name'].split(" ")[0]
+        display_name = user['user_fields']['geckoboard_display_name']
+        agent_name = (display_name or default_name).upper()
+        support_agents[agent_name] = {
+            'status': get_zendesk_talk_status(user['id'])
+        }
+    if DEBUG: print(f"agents: {support_agents}")
+    return support_agents
+def update_schedule_with_availability(schedule):
+    agents = get_zendesk_support_agents()
+    for i in schedule:
+        i.update(agents[i['agent']])
+    if DEBUG: print(f"schedule with online status: {schedule}")
+# END ZENDESK STUFF
 
 
 def lambda_handler(event, context):
     try:
-        CLIENT.ping()
+        GECKO_CLIENT.ping()
     except:
         raise PermissionError('API key invalid.')
     if len(sys.argv) == 2:
         if sys.argv[1] == 'reset':
-            delete_schedule()
+            del_gecko_dataset()
             sys.exit()
     print(f"Getting current schedule state")
     current_schedule = format_dataset(current_agent_statuses())
+    update_schedule_with_availability(current_schedule)
     print(f"Current schedule state:\n{json.dumps(current_schedule)}")
-    set_schedule(current_schedule, SCHEDULE_SCHEMA)
+    set_gecko_dataset(current_schedule)
     print("Dataset updated!")
 
 if __name__ == '__main__':
